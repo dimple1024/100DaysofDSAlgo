0e84798bdee52843615932571cdfdcbea9df3f72
dimple verma <dimple97verma@gmail.com>
2020-11-08 18:22:01 +0530

    Fixed Index out of range error

--- triple_step_using_dp.py
+++ triple_step_using_dp.py
@@ -7,16 +7,14 @@ def triple_step(n,steps):
     steps[0]=1
     steps[1]=1
     steps[2]=2 # child can either go by taking 1 step twice or by hopping directly to step 2
-    for i in range(3,n):
+    for i in range(3,n+1):
         #Note : this can even be generalised saying , child can take upto k steps at a time , then use a for loop
         #upto k for this summation
-        step[i]=step[i-1]+step[i-2]+step[i-3]
-    return step[n]
-
-
+        steps[i]=steps[i-1]+steps[i-2]+steps[i-3]
+    return steps[n]
 
 
 #Driver of the code
 n=int(input())
-steps=[0]*n
+steps=[0]*(n+1)
 print(triple_step(n,steps))
